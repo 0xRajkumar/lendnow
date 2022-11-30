cf19c537392ebd92c1c0bc92af8b1e92a230d3b0
0xRajkumar <rajkumarcoolx@gmail.com>
2022-11-30 20:54:08 +0530

    collateral amount -> collateral amount + borrow amount

--- src/Pool.sol
+++ src/Pool.sol
@@ -47,12 +47,42 @@ contract Pool {
         }
     }
 
+    function toShares(uint256 totalShares, uint256 totalAmount, uint256 amount)
+        internal
+        pure
+        returns (uint256 shares)
+    {
+        if (totalAmount == 0) {
+            shares = amount;
+        } else {
+            shares = (amount * totalShares) / totalAmount;
+            if ((shares * totalAmount) / totalShares < amount) {
+                shares = shares + 1;
+            }
+        }
+    }
+
     function toAmount(Vault memory total, uint256 shares) internal pure returns (uint256 amount) {
         if (total.shares == 0) {
             amount = shares;
         } else {
             amount = (shares * total.amount) / total.shares;
             if ((amount * total.shares) / total.amount < shares) {
+                amount = amount + 1;
+            }
+        }
+    }
+
+    function toAmount(uint256 totalAmount, uint256 totalShares, uint256 shares)
+        internal
+        pure
+        returns (uint256 amount)
+    {
+        if (totalShares == 0) {
+            amount = shares;
+        } else {
+            amount = (shares * totalAmount) / totalShares;
+            if ((amount * totalShares) / totalAmount < shares) {
                 amount = amount + 1;
             }
         }
@@ -66,7 +96,8 @@ contract Pool {
         require(amount > 0, "Invalid amount");
         TokenData storage tokenData = (token == token0) ? token0Data : token1Data;
         IERC20(token).transferFrom(msg.sender, address(this), amount);
-        uint256 inShare = toShares(tokenData.totalCollateral, amount);
+        uint256 totalAmount = tokenData.totalCollateral.amount + tokenData.totalborrow.amount;
+        uint256 inShare = toShares(tokenData.totalCollateral.shares, totalAmount, amount);
         tokenData.totalCollateral.shares = tokenData.totalCollateral.shares + uint128(inShare);
         tokenData.totalCollateral.amount = tokenData.totalCollateral.amount + uint128(amount);
         UserData storage userData = users[msg.sender];
@@ -78,7 +109,8 @@ contract Pool {
         UserData storage userData = users[msg.sender];
         require(userData.collateralBalance >= shareAmount, "Low collateral Balance");
         TokenData storage tokenData = (token == token0) ? token0Data : token1Data;
-        uint256 amount = toAmount(tokenData.totalCollateral, shareAmount);
+        uint256 totalAmount = tokenData.totalCollateral.amount + tokenData.totalborrow.amount;
+        uint256 amount = toAmount(totalAmount, tokenData.totalCollateral.shares, shareAmount);
         IERC20(token).transfer(msg.sender, amount);
         tokenData.totalCollateral.shares = tokenData.totalCollateral.shares - uint128(shareAmount);
         tokenData.totalCollateral.amount = tokenData.totalCollateral.amount - uint128(amount);
